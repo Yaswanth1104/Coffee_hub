35e9b2dbeef27e65263be90b2fa1b281a62bc41f
GUDE YASWANTH <yaswanthgude565@gmail.com>
2026-09-07 23:06:29 +0530

    Stage 5: add responsive scroll-driven motion polish

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import "./dark-luxury.css";
 import "./premium-ui.css";
 import "./premium-account-admin.css";
+import "./premium-scroll.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>
